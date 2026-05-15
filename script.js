84cf5bb72227d3f91decbc998de997508ead5963
COOVT-Marketing <syedshayanabbasr15121472@gmail.com>
2026-05-16 02:49:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
     tf.src = ("https:" == document.location.protocol ? 'https' : 'http') +
         '://api.trustedform.com/trustedform.js?field=xxTrustedFormCertUrl&use_tagged_consent=true&l=' +
         new Date().getTime() + Math.random();
-    var s = document.getElementsByTagName('script')[0];
+    var s = document.getElementsByTagName('script')[0]; 
     s.parentNode.insertBefore(tf, s);
 })();
 
